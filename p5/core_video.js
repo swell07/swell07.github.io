a915dc6c52ef2ec582abc3d5d31585d2d77c9edd
swell07 <danyi@Danyis-MacBook-Pro.local>
2016-04-20 17:20:32 +0200

    line_update

--- p5/core_video.js
+++ p5/core_video.js
@@ -1,11 +1,12 @@
 //---------CLASS PARTICLE------
 // Particle(PVector l, float _r, color _c, float vx, float vy, float ax, float ay, boolean isFRun, ParticleSystem ps)
-function Particle(l, _r, _c, vx, vy, ax, ay, isFRun, ps) {
+function Particle(l, px, py, _r, _c, vx, vy, ax, ay, isFRun, ps) {
     //init begin
     var me = this;
     me.acceleration = createVector(ax, ay); //new PVector(0, random(-0.05, 0.05));
     me.velocity = createVector(vx, vy);
     me.location = l.copy(); //get is deprecated.
+    me.plocation = createVector(px, py);
     me.r = _r * random(3);
     me.isFlowerRun = isFRun;
 
@@ -44,7 +45,8 @@ function Particle(l, _r, _c, vx, vy, ax, ay, isFRun, ps) {
         stroke(0, me.lifespan);
         strokeWeight(0);
         fill(me.c.levels[0], me.c.levels[1], me.c.levels[2], me.lifespan);
-        ellipse(me.location.x, me.location.y, me.r, me.r);
+        //ellipse(me.location.x, me.location.y, me.r, me.r);
+        line(me.location.x, me.location.y, me.plocation.x, me.plocation.y);
     }
 
     me.isDead = function() {
@@ -84,14 +86,14 @@ function ParticleSystem(location, r, colors) {
                 var ax = distance * cos(radians(angle));
                 var ay = distance * sin(radians(angle));
                 var colorchange = (random(1) < 0.5) ? lerpColor(colors[0], colors[1], random(1)) : lerpColor(colors[2], colors[3], random(1));
-                me.particles.push(new Particle(me.origin, me.r, colorchange, vx, vy, ax, ay, true, me));
+                me.particles.push(new Particle(me.origin, me.prevMseX, me.prevMseY, me.r, colorchange, vx, vy, ax, ay, true, me));
             } else {
                 var vx = 0;
                 var vy = random(0, 1);
                 var ax = random(-0.02, 0.04);
                 var ay = random(0.1);
                 var colorchange = (random(1) < 0.5) ? colorchange = lerpColor(colors[0], colors[1], random(1)) : lerpColor(colors[2], colors[3], random(1));
-                me.particles.push(new Particle(me.origin, me.r, colorchange, vx, vy, ax, ay, false, me));
+                me.particles.push(new Particle(me.origin, me.prevMseX, me.prevMseY,me.r, colorchange, vx, vy, ax, ay, false, me));
             }
         }
         if (me.particles.length > 0) {
